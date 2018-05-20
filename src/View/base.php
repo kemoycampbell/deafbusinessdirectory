fe902838e3d93404b790c1419cdcd9eb6f895eed
Ranbir Aulakh <ranbir2010@live.com>
2018-05-20 18:14:55 -0400

    UI changes

--- src/View/base.php
+++ src/View/base.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400%7CRaleway:300,400,500,600,700%7CLato:300,400,400italic,600,700" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
     <link href="{{asset('css/custom.css')}}" rel="stylesheet" type="text/css" />
 
     <title>
@@ -16,30 +16,27 @@
 </head>
 <body>
 
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-expand-lg shadow p-3 mb-3">
     <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">username</a></li>
-                <li><a href="#Mail">Contacts</a></li>
-                <li><a href="#contact"><i class="fa fa-th"></i></a></li>
-                <li><a href="#contact"><i class="fa fa-bell"></i></a></li>
-                <!--<li><a href="#contact"><i class="fa fa-share-alt"></i></a></li>-->
-                <li><a href="#"><i class="fa fa-user"></i></a></li>
-            </ul>
-        </div>
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="#"><i class="fa fa-user"></i> Hello, Username</a>
+                <a class="nav-item nav-link" href="#"><i class="fa fa-sign-out"></i> Logout</a>
+                <a class="nav-item nav-link" href="#"><i class="fa fa-phone"></i> Contact</a>
+                <a class="nav-item nav-link" href="#"><i class="fa fa-sign-in"></i> Login</a>
+                <a class="nav-item nav-link" href="#"><i class="fa fa-user-plus"></i> Register</a>
+            </div>
+        </div>
     </div>
 </nav>
-
 
 {% block content %}
 
@@ -47,35 +44,19 @@ content goes here...
 
 {% endblock %}
 
-<div id="footer">
-    <div class="navbar navbar-default navbar-fixed-bottom" role="navigation">
-        <div class="container">
+<footer class="footer text-muted text-center text-small">
+    <p class="mb-0">© 2017 - 2018 - Deaf Business Directory - All Rights Reserved </p>
+    <ul class="list-inline">
+        <li class="list-inline-item pr-3"><a href="#">Privacy</a></li>
+        <li class="list-inline-item pr-3"><a href="#">Terms</a></li>
+        <li class="list-inline-item pr-3"><a href="#">Support</a></li>
+        <li class="list-inline-item pr-3"><a href="#">FAQ</a></li>
+    </ul>
+</footer>
 
-            <ul class="nav navbar-nav navbar-left">
-                <!--<li><a href="#">Advertising</a></li>
-                <li><a href="#">Business</a></li>-->
-                <li><a href="#">About</a></li>
-                <li><a href="#">Terms</a></li>
-            </ul>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 
-            <ul class="nav navbar-nav navbar-right">
-
-                <li><a href="#">Privacy</a></li>
-                <li><a href="#">Settings</a></li>
-
-
-
-            </ul>
-        </div>
-    </div>
-</div>
-
-
-<script
-        src="https://code.jquery.com/jquery-3.3.1.min.js"
-        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-        crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 {% block javascript %}{% endblock %}
 
 
--- src/View/index.php
+++ src/View/index.php
@@ -3,68 +3,42 @@
 
 {% block content %}
 
-
-<div class="container">
-    <div class="row">
-        <div class="span3"></div>
-        <div class="span3"><h1 class="center-block text-center">Discover the Best Deaf Business</h1></div>
-        <div class="span3"><h4 class="text-center" style="color:white">Deaf Business Directory helps you find all top deaf businesses</h4></div>
-        <div class="span3"></div>
-    </div>
-
-    <div class="row">
-        <div class="col-xs-6">
-            <div class="input-group">
-                <input type="text" class="form-control">
-                <span class="input-group-btn">
-                  <button type="button" class="btn btn-primary" id="google-search">Search</button>
-                </span>
+<br/><br/><br/>
+<div class="container mt-5">
+    <div class="row text-center">
+        <div class="col-md-8 offset-md-2">
+            <h1>Discover the Best Deaf Business</h1>
+            <h4>Deaf Business Directory helps you find all top deaf businesses</h4>
+            <br/>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="button"> <i class="fa fa-search"></i> Search</button>
+                </div>
             </div>
         </div>
     </div>
-    <br/>
-    <br/>
-    <div class="row">
-        <div class="col-lg-3 col-md-4">
+    <br/><br/>
+    <div class="row text-center">
+        <div class="col-md-2 offset-md-4">
             <div class="eff-item">
-                <i class="fa fa-search text-success fa-3x"></i>
-            </div>
-            <div class="eff-tt">
-                <strong>Search</strong>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-4">
-            <div class="eff-item">
-                <i class="fa fa-bookmark-o text-danger fa-3x"></i>
+                <i class="fa fa-star fa-2x text-warning"></i>
             </div>
             <div class="eff-tt">
                 <strong>Bookmark</strong>
             </div>
         </div>
-        <div class="col-lg-3 col-md-4">
+        <div class="col-md-2">
             <div class="eff-item">
-                <i class="fa fa-phone text-info fa-3x"></i>
+                <i class="fa fa-phone fa-2x text-primary"></i>
             </div>
             <div class="eff-tt">
                 <strong>Contact</strong>
             </div>
         </div>
-        <div class="col-lg-3 col-md-4">
-            <div class="eff-item">
-                <i class="fa fa-star-o text-warning fa-3x"></i>
-            </div>
-            <div class="eff-tt">
-                <strong>Review</strong>
-            </div>
-        </div>
+
     </div>
-
-
-    <!--<div class="btn-group-wrap">
-        <div class="btn-group">
-            <button type="button" class="btn btn-primary" id="google-search">Search</button>
-        </div>
-    </div>-->
 </div>
+
 
 {% endblock %}
